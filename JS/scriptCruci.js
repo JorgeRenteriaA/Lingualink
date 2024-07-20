c11ecb6df9d75c021fde34d7135bb8c393bcb33e
JorgeRenteriaA <99306646+JorgeRenteriaA@users.noreply.github.com>
2024-07-20 17:37:43 -0600

    Add files via upload

---
+++ JS/scriptCruci.js
@@ -0,0 +1,24 @@
+function checkAnswers() {
+    // Obtiene todos los elementos de entrada del crucigrama
+    var inputBoxes = document.getElementsByClassName('input-box');
+    var correctCount = 0;
+
+    // Recorre cada elemento de entrada y verifica si coincide con el valor correcto
+    for (var i = 0; i < inputBoxes.length; i++) {
+      var input = inputBoxes[i];
+      var inputValue = input.value.toUpperCase();
+      var correctValue = input.getAttribute('data-word');
+
+      if (inputValue === correctValue) {
+        correctCount++;
+        input.classList.add('correct');
+      } else {
+        input.classList.add('incorrect');
+      }
+    }
+
+    // Muestra el resultado de la calificación
+    var resultElement = document.getElementById('result');
+    resultElement.innerHTML = 'Correct answers: ' + correctCount + '/' + inputBoxes.length+"<br>Contesta la encuesta: <a href='https://forms.gle/5miAj3xvDodtmTaG7'>Encuesta</a>";
+	resultElement.style.display="block";
+  }
---
+++ JS/scripts3.js
@@ -0,0 +1,61 @@
+    function checkAnswers() {
+      var inputs = document.getElementsByTagName('input');
+      var correctAnswers = ["beautifully", "parking", "collection", "friendship", "understandable", "possibility", "refusal", "decoration", "sensitive", "arrival"];
+      var correctCount = 0;
+	  var totalInputs = inputs.length;
+
+      for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.toLowerCase() === correctAnswers[i]) {
+          inputs[i].style.backgroundColor = '#c9fdd8';
+          correctCount++;
+        } else if (inputs[i].value === '') {
+          inputs[i].style.backgroundColor = 'yellow';
+        } else {
+          inputs[i].style.backgroundColor = '#fdd8d8';
+        }
+      }
+
+	var resultContainer = document.getElementById('result-container');
+    resultContainer.style.display = 'block';
+
+
+    var calificacion = (correctCount / totalInputs) * 100;
+
+    var resultText = '<h2>Correct answers: ' + correctCount + '/' + totalInputs + '<h2><br>Score: ' + calificacion + '%';
+
+    resultContainer.innerHTML = resultText + "<br><a href='https://forms.gle/zT8GNyERK2tDcdHM7'>Encuesta</a>";
+    }
+
+	function revisarRespuestas(enlace) {
+      var preguntas = document.getElementsByClassName("question");
+      var totalPreguntas = preguntas.length;
+      var respuestasCorrectas = 0;
+
+      for (var i = 0; i < totalPreguntas; i++) {
+        var pregunta = preguntas[i];
+        var opciones = pregunta.getElementsByTagName("input");
+        var totalOpciones = opciones.length;
+        var respuestaCorrecta = false;
+
+        for (var j = 0; j < totalOpciones; j++) {
+          var opcion = opciones[j];
+
+          if (opcion.checked && opcion.value === "correcta") {
+            respuestaCorrecta = true;
+            break;
+          }
+        }
+
+        if (respuestaCorrecta) {
+          respuestasCorrectas++;
+          pregunta.style.backgroundColor = "#c9fdd8";
+        } else {
+          pregunta.style.backgroundColor = "#fdd8d8";
+        }
+      }
+
+      var calificacion = (respuestasCorrectas / totalPreguntas) * 100;
+      var resultadoDiv = document.getElementById("result");
+      resultadoDiv.innerHTML = "<h2>Respuestas correctas: " + respuestasCorrectas + "/" + totalPreguntas + "<h2><br>Calificación: " + calificacion + "%<br>"+enlace;
+      resultadoDiv.style.display = "block";
+    }
